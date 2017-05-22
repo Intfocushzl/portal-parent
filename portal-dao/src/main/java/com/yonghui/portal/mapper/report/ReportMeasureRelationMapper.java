10d22d7a6ce98e0928024f3b5637770fe8d4086b
zhai <328413963@qq.com>
2017-05-22 17:24:27 +0800

    报表定义

---
+++ portal-dao/src/main/java/com/yonghui/portal/mapper/report/ReportMeasureRelationMapper.java
@@ -0,0 +1,13 @@
+package com.yonghui.portal.mapper.report;
+
+import com.yonghui.portal.mapper.base.BaseMapper;
+import com.yonghui.portal.model.report.ReportMeasureRelation;
+
+/**
+ * @author zhanghai
+ * @email walk_hai@163.com
+ * @date 2017-05-22 16:17:07
+ */
+public interface ReportMeasureRelationMapper extends BaseMapper<ReportMeasureRelation> {
+
+}
