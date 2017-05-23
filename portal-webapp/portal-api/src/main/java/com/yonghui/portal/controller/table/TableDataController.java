121d85af15aafc82b856dfa9b78ad408144f60dc
shengwm <179653076@qq.com>
2017-05-23 11:40:10 +0800

    表头API

--- portal-webapp/portal-api/src/main/java/com/yonghui/portal/controller/table/TableDataController.java
+++ portal-webapp/portal-api/src/main/java/com/yonghui/portal/controller/table/TableDataController.java
@@ -1,6 +1,7 @@
 package com.yonghui.portal.controller.table;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.yonghui.portal.annotation.IgnoreAuth;
 import com.yonghui.portal.service.table.TableDataService;
 import com.yonghui.portal.util.R;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 /**前端获取报表控制类
  * Created by Shengwm on 2017/5/22.
@@ -20,7 +23,9 @@ public class TableDataController {
 
     @RequestMapping("title")
     @ResponseBody
+    @IgnoreAuth
     public R getTableTileByReportCode(HttpServletRequest request,String reportcode){
-        return  R.error();
+        List<Map<String,Object>> maps = tableDataService.getTableTileByReportCode(reportcode);
+        return  R.success(maps);
     }
 }
