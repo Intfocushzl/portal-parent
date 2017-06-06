d5b6bcaf3b9bee347428ea50174c502454a28aaf
liuwei <543335108@qq.com>
2017-06-06 15:44:44 +0800

    app扫码接口调整，商行赛马导出

--- portal-webapp/portal-api/src/main/java/com/yonghui/portal/controller/api/AppQrCodeApiController.java
+++ portal-webapp/portal-api/src/main/java/com/yonghui/portal/controller/api/AppQrCodeApiController.java
@@ -1,11 +1,11 @@
 package com.yonghui.portal.controller.api;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSONObject;
 import com.yonghui.portal.annotation.IgnoreAuth;
-import com.yonghui.portal.util.ConstantsUtil;
-import com.yonghui.portal.util.HttpContextUtils;
-import com.yonghui.portal.util.Md5Util;
-import com.yonghui.portal.util.R;
+import com.yonghui.portal.model.sys.SysOperationLog;
+import com.yonghui.portal.service.sys.SysoperationLogService;
+import com.yonghui.portal.util.*;
 import com.yonghui.portal.util.redis.ReportUtil;
 import com.yonghui.portal.xss.SQLFilter;
 import org.apache.log4j.Logger;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +31,9 @@ import java.util.Map;
 public class AppQrCodeApiController {
 
     Logger log = Logger.getLogger(this.getClass());
+
+    @Reference
+    private SysoperationLogService sysoperationLogService;
 
     @Autowired
     private ReportUtil reportUtil;
@@ -51,6 +55,7 @@ public class AppQrCodeApiController {
         String parameter = null;
         String result = null;
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> shopIdList = new ArrayList<Map<String, Object>>();
         try {
             //首先判断客户端秘钥是否正确
             Md5Util util = new Md5Util();
@@ -60,10 +65,26 @@ public class AppQrCodeApiController {
             }
             //调用自己库查数据
             parameter = HttpContextUtils.getRequestParameter(req);
+            SysOperationLog log = new SysOperationLog();
+            log.setIp(new IPUtils().getIpAddr(req));
+            log.setStartTime(new Date());
+            log.setUrl(req.getRequestURL().toString());
+            log.setParameter(parameter);
+            //首先根据parameter里面的shopId去查  该门店所在区域下下的所有门店
+            shopIdList = reportUtil.jdbcProListResultListMapByParam(SQLFilter.sqlInject("REP_000045"), SQLFilter.sqlInject(parameter));
+            //将查出来的门店结果，拼接成参数
+            StringBuffer newParam = new StringBuffer();
+            for(Map<String, Object> item : shopIdList){
+                newParam.append("'"+item.get("shopId")+"',");
+            }
+            String newParamStr = "shopID="+newParam.toString().substring(1,newParam.toString().length()-2)+"@@barcode="+barcode;
             //注意此处code  admin配置的是98  为了给app扫码统一code（REP_000043）
-            list = reportUtil.jdbcProListResultListMapByParam(SQLFilter.sqlInject("REP_000043"), SQLFilter.sqlInject(parameter));
+            list = reportUtil.jdbcProListResultListMapByParam(SQLFilter.sqlInject("REP_000043"),newParamStr);
             //调用外部接口获取数据
             result = reportUtil.qRResultByParam(parameter, URL);
+            log.setEndTime(new Date());
+            log.setRemark("app@@qrCode");
+            sysoperationLogService.SaveLog(log);
         } catch (Exception e) {
             R.error("执行APP报表存储过程报表异常");
         }
