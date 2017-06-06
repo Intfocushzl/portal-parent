02ae01b1848baddd3038cba3188240cd81247e24
zhai <328413963@qq.com>
2017-06-06 16:07:18 +0800

    报表标题

---
+++ portal-common/src/main/java/com/yonghui/portal/util/GzipUtils.java
@@ -0,0 +1,99 @@
+package com.yonghui.portal.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * <p> Description:压缩工具类 </p>
+ *
+ * @author zhanghai
+ */
+public class GzipUtils {
+
+    /**
+     * <p> Description:使用gzip进行压缩 </p>
+     *
+     * @param val
+     * @return
+     */
+    public static String gzip(String val) {
+        if (val == null || val.length() == 0) {
+            return val;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = null;
+        try {
+            gzip = new GZIPOutputStream(out);
+            gzip.write(val.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (gzip != null) {
+                try {
+                    gzip.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return new sun.misc.BASE64Encoder().encode(out.toByteArray());
+    }
+
+    /**
+     * <p> Description:使用gzip进行解压缩  </p>
+     *
+     * @param zipVal
+     * @return
+     */
+    public static String ungzip(String zipVal) {
+        if (zipVal == null) {
+            return null;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = null;
+        GZIPInputStream ginzip = null;
+        byte[] compressed = null;
+        String decompressed = null;
+        try {
+            compressed = new sun.misc.BASE64Decoder().decodeBuffer(zipVal);
+            in = new ByteArrayInputStream(compressed);
+            ginzip = new GZIPInputStream(in);
+
+            byte[] buffer = new byte[1024];
+            int offset = -1;
+            while ((offset = ginzip.read(buffer)) != -1) {
+                out.write(buffer, 0, offset);
+            }
+            decompressed = out.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ginzip != null) {
+                try {
+                    ginzip.close();
+                } catch (IOException e) {
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        return decompressed;
+    }
+
+}
