84d0eb96bc8f85aa55b9e5d49a9de2fcf6a1dc1e
zhai <328413963@qq.com>
2017-06-16 12:11:47 +0800

    app模板定义

--- portal-webapp/portal-admin/src/main/webapp/js/reportmodulepage/reportmodulepage.js
+++ portal-webapp/portal-admin/src/main/webapp/js/reportmodulepage/reportmodulepage.js
@@ -1,41 +1,62 @@
 $(function () {
     $("#jqGrid").jqGrid({
         url: '../reportmodulepage/list',     // 请求后台json数据的url
-        datatype: "json",                // 后台返回的数据格式
+        datatype: "json",                    // 后台返回的数据格式
         // 列表标题及列表模型
         colModel: [
-            {label: 'id', name: 'id', index: 'id', width: 50, key: true },
-            {label: '专题类型 0：PC专题 1：APP专题', name: 'pageType', index: 'page_type', width: 80 }, 
-            {label: '', name: 'title', index: 'title', width: 80 }, 
-            {label: '原文', name: 'content', index: 'content', width: 80 }, 
-            {label: '保存草稿内容', name: 'designStructrue', index: 'design_structrue', width: 80 }, 
-            {label: '发布正式内容', name: 'onlineStructrue', index: 'online_structrue', width: 80 }, 
-            {label: '1：草稿 2：已发布', name: 'status', index: 'status', width: 80 }, 
-            {label: '创建时间', name: 'createTime', index: 'CREATE_TIME', width: 80 }, 
+            {label: 'id', name: 'id', index: 'id', width: 50, key: true},
+            {
+                label: '专题类型',
+                name: 'pageType',
+                index: 'page_type',
+                width: 80,
+                formatter: function (value, options, row) {
+                    return value === 0 ?
+                        'PC' :
+                        'APP';
+                }
+            },
+            {label: '标题', name: 'title', index: 'title', width: 80},
+            {
+                label: '状态', name: 'status', index: 'status', width: 80,
+                formatter: function (value, options, row) {
+                    return value === 1 ?
+                        '<span class="label label-warning">新草稿</span>' :
+                        '<span class="label label-info">已发布</span>';
+                }
+            },
+            {
+                label: '开关', name: 'disabled', index: 'statdisabledus', width: 80,
+                formatter: function (value, options, row) {
+                    return value === 0 ?
+                        '<span class="label label-success">启用</span>' :
+                        '<span class="label label-danger">禁用</span>';
+                }
+            },
         ],
         viewrecords: true,     // 是否显示行号，默认值是false，不显示
         height: 385,            // 表格高度
         rowNum: 50,             // 一页显示的行记录数
-        rowList : [50,100],     // 翻页控制条中 每页显示记录数可选集合
+        rowList: [50, 100],     // 翻页控制条中 每页显示记录数可选集合
         rownumbers: true,
         rownumWidth: 25,
-        autowidth:true,
+        autowidth: true,
         multiselect: true,
         pager: "#jqGridPager",          // 翻页DOM
-        jsonReader : {                   // 重写后台返回数据的属性
+        jsonReader: {                   // 重写后台返回数据的属性
             root: "page.list",          // 将rows修改为page.list
             page: "page.currPage",      // 将page修改为page.currPage
             total: "page.totalPage",    // 将total修改为page.totalPage
             records: "page.totalCount"  // 将records修改为page.totalCount
         },
-        prmNames : {              // 改写请求参数属性
-            page:"page",
-            rows:"limit",
+        prmNames: {              // 改写请求参数属性
+            page: "page",
+            rows: "limit",
             order: "order"
         },
-        gridComplete:function(){
+        gridComplete: function () {
             //隐藏grid底部滚动条
-            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" });
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
             //设置高度
             $("#jqGrid").jqGrid('setGridHeight', getWinH());
         }
@@ -43,8 +64,8 @@ $(function () {
 });
 
 var vm = new Vue({
-    el:'#rrapp',
-    data:{
+    el: '#rrapp',
+    data: {
         showList: true,
         title: null,
         reportModulePage: {}
@@ -60,15 +81,15 @@ var vm = new Vue({
                 page: 1
             }).trigger("reloadGrid");
         },
-        add: function(){
+        add: function () {
             vm.showList = false;
             vm.title = "新增";
             vm.reportModulePage = {};
         },
         update: function (event) {
             var id = getSelectedRow();
-            if(id == null){
-                return ;
+            if (id == null) {
+                return;
             }
             vm.showList = false;
             vm.title = "修改";
@@ -81,12 +102,12 @@ var vm = new Vue({
                 type: "POST",
                 url: url,
                 data: JSON.stringify(vm.reportModulePage),
-                success: function(r){
-                    if(r.code === 0){
-                        alert('操作成功', function(index){
+                success: function (r) {
+                    if (r.code === 0) {
+                        alert('操作成功', function (index) {
                             vm.reload();
-                     });
-                    }else{
+                        });
+                    } else {
                         alert(r.msg);
                     }
                 }
@@ -94,37 +115,37 @@ var vm = new Vue({
         },
         del: function (event) {
             var ids = getSelectedRows();
-            if(ids == null){
-                return ;
+            if (ids == null) {
+                return;
             }
 
-            confirm('确定要删除选中的记录？', function(){
+            confirm('确定要删除选中的记录？', function () {
                 $.ajax({
                     type: "POST",
                     url: "../reportmodulepage/delete",
                     data: JSON.stringify(ids),
-                    success: function(r){
-                        if(r.code == 0){
-                            alert('操作成功', function(index){
+                    success: function (r) {
+                        if (r.code == 0) {
+                            alert('操作成功', function (index) {
                                 $("#jqGrid").trigger("reloadGrid");
                             });
-                        }else{
+                        } else {
                             alert(r.msg);
                         }
                     }
                 });
             });
         },
-        getInfo: function(id){
-            $.get("../reportmodulepage/info/"+id, function(r){
+        getInfo: function (id) {
+            $.get("../reportmodulepage/info/" + id, function (r) {
                 vm.reportModulePage = r.reportModulePage;
             });
         },
         reload: function (event) {
             vm.showList = true;
-            var page = $("#jqGrid").jqGrid('getGridParam','page');
-            $("#jqGrid").jqGrid('setGridParam',{
-                page:page
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                page: page
             }).trigger("reloadGrid");
         }
     }
