73f8374518ee2b183dd72f4ec568dbbe9bfabb47
zhai <328413963@qq.com>
2017-06-29 15:48:42 +0800

    ftp文件上传

--- portal-webapp/portal-admin/src/main/webapp/js/businessman/businessmanacticlefileinput.js
+++ portal-webapp/portal-admin/src/main/webapp/js/businessman/businessmanacticlefileinput.js
@@ -22,6 +22,7 @@ function initkindEditor() {
             syncType: "",
             filterMode: true,
             allowPreviewEmoticons: false,
+            height: "500px",
             //先注释页面查看所有图标的data-name
             items: [
                 'source', 'undo', 'redo', 'plainpaste', 'wordpaste', 'clearhtml', 'quickformat',
