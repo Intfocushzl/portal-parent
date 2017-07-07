561ddbc4794f6c3dc4aafd4e27d5d415049ec504
zhai <328413963@qq.com>
2017-07-07 10:26:21 +0800

    标签显示，是否启用

--- portal-webapp/portal-admin/src/main/webapp/js/businessman/businessmantaginfo.js
+++ portal-webapp/portal-admin/src/main/webapp/js/businessman/businessmantaginfo.js
@@ -4,34 +4,42 @@ $(function () {
         datatype: "json",                // 后台返回的数据格式
         // 列表标题及列表模型
         colModel: [
-            {label: 'id', name: 'id', index: 'id', width: 50, key: true },
-            {label: '标签名', name: 'tagName', index: 'tag_name', width: 80 }, 
-            {label: '标签类型', name: 'tagType', index: 'tag_type', width: 80 },
-            {label: '创建时间', name: 'createTime', index: 'CREATE_TIME', width: 80 }, 
+            {label: 'id', name: 'id', index: 'id', width: 50, key: true},
+            {label: '标签名', name: 'tagName', index: 'tag_name', width: 80},
+            {
+                label: '标签类型', name: 'tagType', index: 'tag_type', width: 80, formatter: function (type) {
+                if (type == 1) {
+                    return "文章";
+                } else if (type == 2) {
+                    return "视频";
+                }
+            }
+            },
+            {label: '创建时间', name: 'createTime', index: 'CREATE_TIME', width: 80},
         ],
         viewrecords: true,     // 是否显示行号，默认值是false，不显示
         height: 385,            // 表格高度
         rowNum: 50,             // 一页显示的行记录数
-        rowList : [50,100],     // 翻页控制条中 每页显示记录数可选集合
+        rowList: [50, 100],     // 翻页控制条中 每页显示记录数可选集合
         rownumbers: true,
         rownumWidth: 25,
-        autowidth:true,
+        autowidth: true,
         multiselect: true,
         pager: "#jqGridPager",          // 翻页DOM
-        jsonReader : {                   // 重写后台返回数据的属性
+        jsonReader: {                   // 重写后台返回数据的属性
             root: "page.list",          // 将rows修改为page.list
             page: "page.currPage",      // 将page修改为page.currPage
             total: "page.totalPage",    // 将total修改为page.totalPage
             records: "page.totalCount"  // 将records修改为page.totalCount
         },
-        prmNames : {              // 改写请求参数属性
-            page:"page",
-            rows:"limit",
+        prmNames: {              // 改写请求参数属性
+            page: "page",
+            rows: "limit",
             order: "order"
         },
-        gridComplete:function(){
+        gridComplete: function () {
             //隐藏grid底部滚动条
-            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" });
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
             //设置高度
             $("#jqGrid").jqGrid('setGridHeight', getWinH());
         }
@@ -39,8 +47,8 @@ $(function () {
 });
 
 var vm = new Vue({
-    el:'#rrapp',
-    data:{
+    el: '#rrapp',
+    data: {
         showList: true,
         title: null,
         businessmanTagInfo: {}
@@ -56,15 +64,15 @@ var vm = new Vue({
                 page: 1
             }).trigger("reloadGrid");
         },
-        add: function(){
+        add: function () {
             vm.showList = false;
             vm.title = "新增";
             vm.businessmanTagInfo = {};
         },
         update: function (event) {
             var id = getSelectedRow();
-            if(id == null){
-                return ;
+            if (id == null) {
+                return;
             }
             vm.showList = false;
             vm.title = "修改";
@@ -77,12 +85,12 @@ var vm = new Vue({
                 type: "POST",
                 url: url,
                 data: JSON.stringify(vm.businessmanTagInfo),
-                success: function(r){
-                    if(r.code === 0){
-                        alert('操作成功', function(index){
+                success: function (r) {
+                    if (r.code === 0) {
+                        alert('操作成功', function (index) {
                             vm.reload();
-                     });
-                    }else{
+                        });
+                    } else {
                         alert(r.msg);
                     }
                 }
@@ -90,37 +98,37 @@ var vm = new Vue({
         },
         del: function (event) {
             var ids = getSelectedRows();
-            if(ids == null){
-                return ;
+            if (ids == null) {
+                return;
             }
 
-            confirm('确定要删除选中的记录？', function(){
+            confirm('确定要删除选中的记录？', function () {
                 $.ajax({
                     type: "POST",
                     url: "../businessmantaginfo/delete",
                     data: JSON.stringify(ids),
-                    success: function(r){
-                        if(r.code == 0){
-                            alert('操作成功', function(index){
+                    success: function (r) {
+                        if (r.code == 0) {
+                            alert('操作成功', function (index) {
                                 $("#jqGrid").trigger("reloadGrid");
                             });
-                        }else{
+                        } else {
                             alert(r.msg);
                         }
                     }
                 });
             });
         },
-        getInfo: function(id){
-            $.get("../businessmantaginfo/info/"+id, function(r){
+        getInfo: function (id) {
+            $.get("../businessmantaginfo/info/" + id, function (r) {
                 vm.businessmanTagInfo = r.businessmanTagInfo;
             });
         },
         reload: function (event) {
             vm.showList = true;
-            var page = $("#jqGrid").jqGrid('getGridParam','page');
-            $("#jqGrid").jqGrid('setGridParam',{
-                page:page
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                page: page
             }).trigger("reloadGrid");
         }
     }
