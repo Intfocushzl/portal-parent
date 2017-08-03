c247ef464625577f715c78601e1e4dae867791ef
Intfocushzl <519341988@qq.com>
2017-08-03 20:12:21 +0800

    fix: 1.(报表开放接口文档-APP日复盘)；日复盘模块后台接口文档 2.(StoreReplayServiceImpl.java):BUG 修复

--- portal-api-service-parent/portal-api-service/src/main/java/com/yonghui/portal/service/impl/storeReplay/StoreReplayServiceImpl.java
+++ portal-api-service-parent/portal-api-service/src/main/java/com/yonghui/portal/service/impl/storeReplay/StoreReplayServiceImpl.java
@@ -144,7 +144,7 @@ public class StoreReplayServiceImpl implements StoreRePlayService {
             }
 
             //1.小店回复
-            sql = createSql.getEvaluateListByRole("44", areaName, createdAt.replace("/", ""));
+            sql = createSql.getEvaluateListByRole("43", areaName, createdAt.replace("/", ""));
             list = getBaseList(sql, portalDataSource);
             for (Map<String, Object> mapObj : listActionXd) {
                 List<Map<String, Object>> listEvaluatesXd = new ArrayList<>();
@@ -161,7 +161,7 @@ public class StoreReplayServiceImpl implements StoreRePlayService {
              }
 
             //2.品类教练回复 评论
-            sql = createSql.getEvaluateListByRole("43,111", areaName, createdAt.replace("/", ""));
+            sql = createSql.getEvaluateListByRole("111", areaName, createdAt.replace("/", ""));
             list = getBaseList(sql, portalDataSource);
             for (Map<String, Object> mapObj : listActionPj) {
                 List<Map<String, Object>> listEvaluatesPj = new ArrayList<>();
@@ -177,7 +177,7 @@ public class StoreReplayServiceImpl implements StoreRePlayService {
             }
 
             //3.个人所有行动方案的评论
-            sql = createSql.getEvaluateList(userId, null, createdAt.replace("/", ""));
+            sql = createSql.getEvaluateList(null, areaName, createdAt.replace("/", ""));
             list = getBaseList(sql, portalDataSource);
             for (Map<String, Object> mapObj : listActionGr) {
                 List<Map<String, Object>> listEvaluatesGr = new ArrayList<>();
